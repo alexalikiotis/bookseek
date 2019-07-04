87eafcac3bebf12b70192fd5bcab8bba191b0b32
alexalikiotis <alexalikiotis5@gmail.com>
2019-07-04 14:29:46 +0300

    Rendering appropriate UI when library is empty and cleaning up inline css in library screen

--- src/screens/Library.js
+++ src/screens/Library.js
@@ -91,43 +91,36 @@ const Library = () => {
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.safeArea}>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            paddingHorizontal: 15,
-            paddingVertical: 10,
-          }}
-        >
+        <View style={styles.header}>
           <Text style={styles.title}>Library</Text>
           <Icon
             name="md-arrow-round-forward"
-            style={{ position: 'relative', top: 5 }}
+            style={styles.headerIcon}
             size={35}
             color="#bdc3c7"
           />
         </View>
-        <FlatList
-          data={[{}]}
-          keyExtractor={(_, index) => index.toString()}
-          renderItem={() => (
-            <View
-              style={{
-                flex: 1,
-                display: 'flex',
-                flexDirection: 'row',
-                flexWrap: 'wrap',
-                marginHorizontal: 5,
-              }}
-            >
-              {books.map((item, index) => (
-                <BookCard key={index} data={item} />
-              ))}
-            </View>
-          )}
-        />
+        {books.length === 0 ? (
+          <View style={styles.emptyLibrary}>
+            <Icon name="ios-folder-open" color="#ecf0f1" size={150} />
+            <Text style={styles.emptyLibraryTitle}>Hmmm,</Text>
+            <Text style={styles.emptyLibraryText}>
+              looks like your library is empty
+            </Text>
+          </View>
+        ) : (
+          <FlatList
+            data={[{}]}
+            keyExtractor={(_, index) => index.toString()}
+            renderItem={() => (
+              <View style={styles.libraryContent}>
+                {books.map((item, index) => (
+                  <BookCard key={index} data={item} />
+                ))}
+              </View>
+            )}
+          />
+        )}
       </SafeAreaView>
     </View>
   );
@@ -140,9 +133,45 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
   },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  headerIcon: {
+    position: 'relative',
+    top: 5,
+  },
   title: {
     fontSize: 35,
     fontWeight: 'bold',
+  },
+  emptyLibrary: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emptyLibraryTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#bdc3c7',
+    marginVertical: 20,
+  },
+  emptyLibraryText: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: '#bdc3c7',
+  },
+  libraryContent: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginHorizontal: 5,
   },
 });
 
